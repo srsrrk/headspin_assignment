692828002217c3f1cc3ff0ff7f73dbc2c3bc60b0
srsrrk <83409950+srsrrk@users.noreply.github.com>
2021-04-30 20:06:31 +0530

    Update reverse string.py

--- reverse string.py
+++ reverse string.py
@@ -1,5 +1,5 @@
 # reverse.string
-string=input(" enter a string with dots as separater : ")
+string=input(" enter a string with dots as separator : ")
 words = string.split(".")
 words = list(reversed(words))
 print(".".join(words))
